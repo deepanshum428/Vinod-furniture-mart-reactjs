9534233f454c18b5c42dd36e33df55459028742b
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-07-23 23:10:12 +0530

    create home page for layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="" element={<Home />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Route>
     )
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
-import HeroSection from "./components/HeroSection/HeroSection.jsx";
-import BestSellerSection from "./components/BestSellerSection/BestSellerSection.jsx";
-import FeatureSection from "./components/FeatureSection/FeatureSection.jsx";
-import CategoriesSection from "./components/CategoriesSection/CategoriesSection.jsx";
-import ShopLookSection from "./components/ShopLookSection/ShopLookSection.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 
 const Layout = (props) => {
   return (
     <div>
       <Header />
-      <HeroSection />
-      <BestSellerSection />
-      <FeatureSection />
-      <CategoriesSection />
-      <ShopLookSection />
       <Outlet />
       <Footer />
     </div>
