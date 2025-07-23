823f1623fc127ed66ba20c7bcc5bab7543b2a26d
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-07-23 19:38:20 +0530

    add feature section and categories section

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,8 @@ import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
 import HeroSection from "./components/HeroSection/HeroSection.jsx";
 import BestSellerSection from "./components/BestSellerSection/BestSellerSection.jsx";
+import FeatureSection from "./components/FeatureSection/FeatureSection.jsx";
+import CategoriesSection from "./components/CategoriesSection/CategoriesSection.jsx";
 
 const Layout = (props) => {
   return (
@@ -11,6 +13,8 @@ const Layout = (props) => {
       <Header />
       <HeroSection />
       <BestSellerSection />
+      <FeatureSection />
+      <CategoriesSection />
       <Outlet />
       <Footer />
     </div>
