67cdef204b06015b14098424ad5222327c827578
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-07-24 22:29:47 +0530

    add route on about page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
 import ErrorPage from "./pages/ErrorPage.jsx";
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
+import About from "./pages/About.jsx";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products" element={<Products />}></Route>
+        <Route path="our-story" element={<About />}></Route>
         <Route path="" element={<Home />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Route>
