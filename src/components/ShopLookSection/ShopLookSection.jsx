04f2acf91bd57dcb99a68273673de98f95679c55
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-07-25 20:28:16 +0530

    start left arrow and right aroow in working

--- src/components/ShopLookSection/ShopLookSection.jsx
+++ src/components/ShopLookSection/ShopLookSection.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { ArrowLeft, ArrowRight } from "lucide-react";
-import bgIMG from "../../assets/shopLookBgImg.avif";
+import IMG from "../../assets/shopLookBgImg.avif";
+import IMG2 from "../../assets/shopLookBgImg2.avif";
+import IMG3 from "../../assets/shopLookBgImg3.avif";
+
+const Items = [IMG, IMG2, IMG3];
 
 const ShopLookSection = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % Items.length);
+  };
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) => {
+      return prevIndex === 0 ? Items.length - 1 : prevIndex - 1;
+    });
+  };
+  const activeItem = Items[activeIndex];
+  console.log("Active Index", activeIndex);
+
   return (
     <div className="w-full bg-white p-6">
       <h2 className="text-2xl font-semibold mb-4">Shop the look</h2>
       <div className="relative w-full h-[500px] rounded overflow-hidden shadow">
-        <img src={bgIMG} alt="Room" className="w-full h-full object-cover" />
+        <img
+          src={activeItem}
+          alt="Room"
+          className="w-full h-full object-cover"
+        />
 
         {/* Arrows */}
         <div className="absolute top-4 right-4 flex gap-3 z-30">
           <button className="w-9 h-9 rounded-full bg-white shadow flex items-center justify-center cursor-pointer">
-            <ArrowLeft size={16} />
+            <ArrowLeft size={16} onClick={handlePrev} />
           </button>
           <button className="w-9 h-9 rounded-full bg-white shadow flex items-center justify-center cursor-pointer">
-            <ArrowRight size={16} />
+            <ArrowRight size={16} onClick={handleNext} />
           </button>
         </div>
       </div>
