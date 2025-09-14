c91929240b3ad067be6b00b80cc5268d1a993040
Deepanshu Malviya <deepanshumalviya428@gmail.com>
2025-09-14 22:38:04 +0530

    use window.scrollTo for scroll on top onchange of every page and instant when same page hard refresh

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import { useEffect } from "react";
 
 const Layout = (props) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(
+      {
+        top: 0,
+        left: 0,
+        behavior: "instant",
+      },
+      [location.pathname]
+    );
+    console.log(location.pathname);
+  });
+
   return (
     <div>
       <Header />
